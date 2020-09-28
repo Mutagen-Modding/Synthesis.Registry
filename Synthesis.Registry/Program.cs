6ad1c3882f5111ea08d568f60ac4f53f72963e98
Justin Swanson <justin.c.swanson@gmail.com>
2020-09-28 17:13:32 -0500

    Initial test project

---
+++ Synthesis.Registry/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GitHubDependents;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Synthesis.Registry
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<Dependent> list = await GitHubDependents.GitHubDependents.GetDependents("noggog", "synthesis", packageID: "UGFja2FnZS0xMzg1MjY1MjYz", pages: byte.MaxValue);
+
+            foreach (var target in list)
+            {
+                System.Console.WriteLine(target.Repository);
+            }
+        }
+    }
+}
