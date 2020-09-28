36ba0537718f72cd77111357540a5ba726bc2264
Justin Swanson <justin.c.swanson@gmail.com>
2020-09-28 17:38:27 -0500

    Export as json file

--- Synthesis.Registry/Program.cs
+++ Synthesis.Registry/Program.cs
@@ -2,6 +2,8 @@
 using GitHubDependents;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
 namespace Synthesis.Registry
 {
@@ -10,11 +12,15 @@ namespace Synthesis.Registry
         static async Task Main(string[] args)
         {
             List<Dependent> list = await GitHubDependents.GitHubDependents.GetDependents("noggog", "synthesis", packageID: "UGFja2FnZS0xMzg1MjY1MjYz", pages: byte.MaxValue);
-
+            if (list.Count == 0) return;
             foreach (var target in list)
             {
-                System.Console.WriteLine(target.Repository);
+                System.Console.WriteLine($"{target.User}/{target.Repository}");
             }
+            File.WriteAllText("patchers.txt", JsonSerializer.Serialize(list, new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            }));
         }
     }
 }
