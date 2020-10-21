b980290c47042ee3b8d5608a5388561d10ae2569
Justin Swanson <justin.c.swanson@gmail.com>
2020-10-21 16:57:41 -0500

    Refactor /w retry on project query

--- Synthesis.Registry/Program.cs
+++ Synthesis.Registry/Program.cs
@@ -17,27 +17,15 @@ namespace Synthesis.Registry
     {
         static async Task Main(string[] args)
         {
-            // Retrieve repositories implementing Mutagen.Bethesda.Synthesis
-            List<Dependent> list = await GitHubDependents.GitHubDependents.GetDependents("noggog", "synthesis", packageID: "UGFja2FnZS0xMzg1MjY1MjYz", pages: byte.MaxValue);
-            if (list.Count == 0)
-            {
-                System.Console.Error.WriteLine("No repositories retrieved!");
-                return;
-            }
-            System.Console.WriteLine("Retrieved repositories:");
-            foreach (var target in list)
-            {
-                System.Console.WriteLine($"  {target}");
-            }
-            System.Console.WriteLine();
-            System.Console.WriteLine();
+            var list = await GetGithubDependencies();
+            if (list.Failed) return;
 
             // Populate metadata about each repository
             var gitHubClient = new GitHubClient(new ProductHeaderValue("SynthesisScraper"));
             var loginToken = args[0];
             gitHubClient.Credentials = new Credentials(loginToken);
             HttpClient client = new HttpClient();
-            var repos = (await Task.WhenAll(list
+            var repos = (await Task.WhenAll(list.Value
                 .Where(dep =>
                 {
                     if (string.IsNullOrWhiteSpace(dep.Repository))
@@ -55,71 +43,32 @@ namespace Synthesis.Registry
                 .Select(async dep =>
                 {
                     System.Console.WriteLine($"Processing {dep}");
-                    var patchers = Array.Empty<PatcherListing>();
-                    try
-                    {
-                        // Get project files within repo
-                        var repoColl = new RepositoryCollection();
-                        repoColl.Add(dep.User, dep.Repository);
-                        var projs = await gitHubClient.Search.SearchCode(new SearchCodeRequest()
-                        {
-                            Extensions = new string[] { "csproj" },
-                            Repos = repoColl
-                        });
+                    var projs = await QueryForProjects(dep, gitHubClient);
 
-                        // Construct listings
-                        patchers = (await Task.WhenAll(projs.Items
-                            .Select(async proj =>
-                            {
-                                var listing = new PatcherListing()
-                                {
-                                    ProjectPath = proj.Path,
-                                };
-                                try
-                                {
-                                    var metaPath = Path.Combine(Path.GetDirectoryName(proj.Path)!, Constants.MetaFileName);
-                                    var content = await gitHubClient.Repository.Content.GetAllContents(dep.User, dep.Repository, metaPath);
-                                    if (content.Count != 1) return null;
-                                    var customization = JsonSerializer.Deserialize<PatcherCustomization>(content[0].Content);
-                                    if (string.IsNullOrWhiteSpace(customization.Nickname))
-                                    {
-                                        customization.Nickname = $"{dep.User}/{dep.Repository}";
-                                    }
-                                    listing.Customization = customization;
-                                }
-                                catch (Octokit.NotFoundException)
-                                {
-                                }
-                                catch (Exception ex)
-                                {
-                                    System.Console.WriteLine($"Error parsing listing for {dep.User}/{dep.Repository} {proj.Path}: {ex}");
-                                    return null;
-                                }
-                                return listing;
-                            })))
-                            .NotNull()
-                            .ToArray();
-                    }
-                    catch (Exception ex)
-                    {
-                        System.Console.WriteLine($"Error processing {dep.User}/{dep.Repository}: {ex}");
-                    }
+                    // Construct listings
+                    var patchers = await ConstructListings(dep, gitHubClient, projs);
 
                     return new RepositoryListing()
                     {
                         AvatarURL = dep.AvatarURL,
                         Repository = dep.Repository!,
                         User = dep.User!,
-                        Patchers = patchers
+                        Patchers = patchers.ToArray()
                     };
                 })))
-                .Where(r => r.Patchers.Length > 0)
+                .Where(r =>
+                {
+                    if (r.Patchers.Length == 0)
+                    {
+                        System.Console.WriteLine($"{r.Repository} skipped because it had no listed patchers.");
+                        return false;
+                    }
+                    return true;
+                })
                 .ToArray();
             var limits = gitHubClient.GetLastApiInfo().RateLimit;
             System.Console.WriteLine($"API usage remaining: {(100d * limits.Remaining / (limits.Limit == 0 ? -1 : limits.Limit))}% ({limits.Remaining}/{limits.Limit})");
             System.Console.WriteLine($"Reset at {limits.Reset}");
-
-            System.Console.Error.WriteLine($"Test");
 
             // Write out final listing
             File.WriteAllText("mutagen-automatic-listing.json",
@@ -132,6 +81,85 @@ namespace Synthesis.Registry
                     {
                         WriteIndented = true
                     }));
+        }
+
+        private static async Task<GetResponse<List<Dependent>>> GetGithubDependencies()
+        {
+            var list = await GitHubDependents.GitHubDependents.GetDependents("noggog", "synthesis", packageID: "UGFja2FnZS0xMzg1MjY1MjYz", pages: byte.MaxValue);
+            if (list.Count == 0)
+            {
+                System.Console.Error.WriteLine("No repositories retrieved!");
+                return GetResponse<List<Dependent>>.Failure;
+            }
+            System.Console.WriteLine("Retrieved repositories:");
+            foreach (var target in list)
+            {
+                System.Console.WriteLine($"  {target}");
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+            return GetResponse<List<Dependent>>.Succeed(list);
+        }
+
+        private static async Task<IEnumerable<string>> QueryForProjects(Dependent dep, GitHubClient gitHubClient)
+        {
+            var repoColl = new RepositoryCollection();
+            repoColl.Add(dep.User, dep.Repository);
+            SearchCodeResult? projs = null;
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    projs = await gitHubClient.Search.SearchCode(new SearchCodeRequest()
+                    {
+                        Extensions = new string[] { "csproj" },
+                        Repos = repoColl
+                    });
+                    if (!projs.IncompleteResults) break;
+                    System.Console.WriteLine($"{dep} failed to retrieve patcher listings.  Trying again");
+                }
+                catch (HttpRequestException)
+                {
+                    System.Console.WriteLine($"{dep} failed to retrieve patcher listings.  Trying again");
+                }
+            }
+
+            if (projs?.IncompleteResults ?? true)
+            {
+                throw new ArgumentException($"Failed to retrieve patcher listings for {dep}");
+            }
+
+            return projs.Items.Select(i => i.Path);
+        }
+
+        private static async Task<IEnumerable<PatcherListing>> ConstructListings(Dependent dep, GitHubClient gitHubClient, IEnumerable<string> projs)
+        {
+            return (await Task.WhenAll(projs
+                .Select(async proj =>
+                {
+                    var listing = new PatcherListing()
+                    {
+                        ProjectPath = proj,
+                    };
+                    try
+                    {
+                        var metaPath = Path.Combine(Path.GetDirectoryName(proj)!, Constants.MetaFileName);
+                        var content = await gitHubClient.Repository.Content.GetAllContents(dep.User, dep.Repository, metaPath);
+                        if (content.Count != 1) return null;
+                        var customization = JsonSerializer.Deserialize<PatcherCustomization>(content[0].Content);
+                        if (string.IsNullOrWhiteSpace(customization.Nickname))
+                        {
+                            customization.Nickname = $"{dep.User}/{dep.Repository}";
+                        }
+                        listing.Customization = customization;
+                    }
+                    catch (Octokit.NotFoundException)
+                    {
+                    }
+                    return listing;
+                })))
+                .NotNull()
+                .ToArray();
         }
     }
 }
