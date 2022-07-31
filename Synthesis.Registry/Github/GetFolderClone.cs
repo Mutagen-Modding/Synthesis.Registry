94ef20fa01b7df4d579df5271ea0d3170850191d
Justin Swanson <justin.c.swanson@gmail.com>
2022-07-30 22:33:08 -0500

    QueryForProjects clones git repo

---
+++ Synthesis.Registry/Github/GetFolderClone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using LibGit2Sharp;
+using Noggog;
+using Synthesis.Registry.MutagenScraper.Dto;
+
+namespace Synthesis.Registry.MutagenScraper.Github;
+
+public class GetFolderClone
+{
+    private readonly IFileSystem _fileSystem;
+    private readonly DirectoryPath _path = $"./Repositories";
+    private readonly HashSet<DirectoryPath> _requested = new();
+
+    public GetFolderClone(
+        IFileSystem fileSystem)
+    {
+        _fileSystem = fileSystem;
+    }
+    
+    public DirectoryPath Get(Listing listing)
+    {
+        var repoPath = Path.Combine(_path, listing.User, listing.Repository);
+        if (_requested.Contains(repoPath)) return repoPath;
+
+        _fileSystem.Directory.DeleteEntireFolder(repoPath);
+        var ret = Repository.Clone($"https://github.com/{listing.User}/{listing.Repository}", repoPath);
+        
+        _requested.Add(repoPath);
+        return repoPath;
+    }
+}
--- Synthesis.Registry/QueryForProjects.cs
+++ Synthesis.Registry/QueryForProjects.cs
@@ -1,90 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Octokit;
 using Synthesis.Registry.MutagenScraper.Dto;
 using Synthesis.Registry.MutagenScraper.Github;
-using Synthesis.Registry.MutagenScraper.Listings;
 
 namespace Synthesis.Registry.MutagenScraper
 {
     public class QueryForProjects
     {
-        private readonly GithubClientProvider _githubClientProvider;
-        private readonly ApiUsagePrinter _apiUsagePrinter;
+        private readonly IFileSystem _fileSystem;
+        private readonly GetFolderClone _getFolderClone;
 
         public QueryForProjects(
-            GithubClientProvider githubClientProvider,
-            ApiUsagePrinter apiUsagePrinter)
+            IFileSystem fileSystem,
+            GetFolderClone getFolderClone)
         {
-            _githubClientProvider = githubClientProvider;
-            _apiUsagePrinter = apiUsagePrinter;
+            _fileSystem = fileSystem;
+            _getFolderClone = getFolderClone;
         }
         
         public async Task<IEnumerable<string>> Query(Listing dep)
         {
-            var repoColl = new RepositoryCollection();
-            repoColl.Add(dep.User, dep.Repository);
-            SearchCodeResult? projs = null;
-            for (int i = 0; i < 10; i++)
-            {
-                try
-                {
-                    System.Console.WriteLine($"{dep} searching for project files");
-                    projs = await _githubClientProvider.Client.Search.SearchCode(new SearchCodeRequest()
-                    {
-                        Extensions = new string[] { "csproj" },
-                        Repos = repoColl
-                    });
-                    await PrintAndWaitForReset();
-                    if (projs.IncompleteResults)
-                    {
-                        System.Console.WriteLine($"{dep} failed to retrieve contained projects.  Trying again");
-                    }
-                    else if (projs.Items.Count == 0)
-                    {
-                        System.Console.WriteLine($"{dep} contained projects returned zero.  Trying again");
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    await Task.Delay(5000);
-                }
-                catch (HttpRequestException)
-                {
-                    System.Console.WriteLine($"{dep} failed to retrieve patcher listings.  Trying again");
-                }
-            }
+            var clonePath = _getFolderClone.Get(dep);
 
-            if (projs?.IncompleteResults ?? true)
-            {
-                throw new ArgumentException($"{dep} failed to retrieve patcher listings");
-            }
-
-            var ret = projs.Items
-                .OrderBy(i => i.Name)
-                .Select(i => i.Path)
+            var projs = _fileSystem.Directory.GetFiles(clonePath, "*.csproj", SearchOption.AllDirectories);
+            
+            var ret = projs
+                .OrderBy(Path.GetFileName)
                 .ToArray();
             System.Console.WriteLine($"{dep} retrieved project files:{Environment.NewLine}   {string.Join($"{Environment.NewLine}   ", ret)}");
             return ret;
-        }
-
-        private async Task PrintAndWaitForReset(bool printReset = false)
-        {
-            var limits = _apiUsagePrinter.Print(printReset);
-            if (limits is { Remaining: 0 })
-            {
-                var millis = (int)(limits.Reset - DateTime.Now).TotalMilliseconds;
-                millis += 2000;
-                if (millis > 0)
-                {
-                    System.Console.WriteLine($"Waiting {millis}ms for API to reset");
-                    await Task.Delay(millis);
-                }
-            }
         }
     }
 }
