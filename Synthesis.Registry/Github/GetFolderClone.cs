d3abe086d5d8fb7c43c622fecd1e56a70daa4832
Justin Swanson <justin.c.swanson@gmail.com>
2022-08-08 22:31:35 -0500

    regenerate listings

--- Synthesis.Registry/Github/GetFolderClone.cs
+++ Synthesis.Registry/Github/GetFolderClone.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Threading;
 using Noggog;
 using Noggog.GitRepository;
 using Synthesis.Registry.MutagenScraper.Dto;
@@ -15,12 +14,19 @@ public interface IGetFolderClone
 public class GetFolderClone : IGetFolderClone
 {
     private readonly ICheckOrCloneRepo _checkOrCloneRepo;
+    private readonly IProvideRepositoryCheckouts _repositoryCheckouts;
+    private readonly IResetToLatestMain _resetToLatestMain;
     private readonly DirectoryPath _path = $"./Repositories";
     private readonly Dictionary<InternalRepositoryListing, DirectoryPath> _cached = new();
 
-    public GetFolderClone(ICheckOrCloneRepo checkOrCloneRepo)
+    public GetFolderClone(
+        ICheckOrCloneRepo checkOrCloneRepo,
+        IProvideRepositoryCheckouts repositoryCheckouts,
+        IResetToLatestMain resetToLatestMain)
     {
         _checkOrCloneRepo = checkOrCloneRepo;
+        _repositoryCheckouts = repositoryCheckouts;
+        _resetToLatestMain = resetToLatestMain;
     }
     
     public DirectoryPath Get(InternalRepositoryListing repositoryListing)
@@ -32,6 +38,12 @@ public class GetFolderClone : IGetFolderClone
             var localDir = Path.Combine(_path, repositoryListing.User, repositoryListing.Repository);
             var result = _checkOrCloneRepo.Check(remoteDir, localDir);
             var ret = result.EvaluateOrThrow();
+
+            using (var repoCheckout = _repositoryCheckouts.Get(ret.Local))
+            {
+                var repo = repoCheckout.Repository;
+                _resetToLatestMain.TryReset(repo);
+            }
             val = ret.Local;
             _cached[repositoryListing] = val;
             return val;
