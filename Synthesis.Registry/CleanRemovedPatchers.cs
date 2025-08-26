132102d039bdb445fc62f813b2392c5829d4c4c1
Justin Swanson <justin.c.swanson@gmail.com>
2025-08-26 00:47:00 -0500

    Don't remove patchers if they dont 404

--- Synthesis.Registry/CleanRemovedPatchers.cs
+++ Synthesis.Registry/CleanRemovedPatchers.cs
@@ -1,5 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Noggog;
 using Synthesis.Bethesda.DTO;
 using Synthesis.Registry.MutagenScraper.Listings;
 
@@ -21,11 +27,48 @@ public class CleanRemovedPatchers
         var listedSet = listed
             .Select(x => new ListingKey(x.User!, x.Repository!))
             .ToHashSet();
+
+        IReadOnlySet<ListingKey> removed;
+        using (HttpClient client = new HttpClient())
+        {
+            
+            var waitLock = new SemaphoreSlim(5); // max 5 concurrent requests
+            var test = await ReturnIfMissing(client, new ListingKey("Noggog", "Mutagen"), waitLock);
+            removed = (await Task.WhenAll(existingListings.Repositories
+                    .Select(x => new ListingKey(x.User, x.Repository))
+                .Where(x => !listedSet.Contains(x))
+                .Select(x => Task.Run(() => ReturnIfMissing(client, x, waitLock)))))
+                .WhereNotNull()
+                .ToHashSet();
+        }
+
         return new MutagenPatchersListing()
         {
             Repositories = existingListings.Repositories
-                .Where(x => listedSet.Contains(new ListingKey(x.User, x.Repository)))
+                .Where(x => !removed.Contains(new ListingKey(x.User, x.Repository)))
                 .ToArray()
         };
+    }
+
+    private async Task<ListingKey?> ReturnIfMissing(HttpClient client, ListingKey listing, SemaphoreSlim waitLock)
+    {
+        await waitLock.WaitAsync();
+        
+        var url = $"https://github.com/{listing.User}/{listing.Repository}";
+        
+        HttpResponseMessage response = await client.SendAsync(
+            new HttpRequestMessage(HttpMethod.Head, url)
+        );
+
+        // Don't spam
+        await Task.Delay(250);
+
+        if (response.StatusCode != HttpStatusCode.NotFound)
+        {
+            return null;
+        }
+        
+        Console.WriteLine("Removing missing patcher: " + url);
+        return listing;
     }
 }
